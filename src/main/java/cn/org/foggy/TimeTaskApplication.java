5056de5a03b731fafb7504164cbd9b106a7ea3d4
张威峰 <zhangweifeng@haiercash.com>
2020-08-17 18:00:00 +0800

    code commit

--- src/main/java/cn/org/foggy/TimeTaskApplication.java
+++ src/main/java/cn/org/foggy/TimeTaskApplication.java
@@ -3,10 +3,7 @@ package cn.org.foggy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @EnableScheduling
 @SpringBootApplication
@@ -15,15 +12,6 @@ public class TimeTaskApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(TimeTaskApplication.class, args);
         System.out.println("TimeTaskApplication is running context: " + context);
-    }
-
-    @Bean
-    public TaskScheduler poolScheduler() {
-        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setThreadNamePrefix("ThreadPoolTaskScheduler");
-        scheduler.setPoolSize(1);
-        scheduler.initialize();
-        return scheduler;
     }
 
 }
